cb3231803a5d524f864ce53ea827cb9a7dc1298c
Loc Nguyen <lhn136@gmail.com>
2021-08-11 22:01:32 -0700

    [fix] styling

--- pages/hospital-near.js
+++ pages/hospital-near.js
@@ -1,9 +1,15 @@
 import { gql } from '@apollo/client';
 import client from '../apollo-client';
 import Hospitals from '../components/Hospitals';
+import styles from '../styles/Home.module.css';
+import instructionImage from '../public/instructions.png';
+import Image from 'next/image';
+import { isNonEmptyArray } from '@apollo/client/utilities';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context) {
   let { latitude, longitude } = context.query;
+
   const QUERY = gql`
     query getHospitals {
       search(
@@ -47,5 +53,68 @@ export async function getServerSideProps(context) {
 }
 
 export default function HospitalNear({ hospitalData }) {
-  return <Hospitals hospitalData={hospitalData} />;
+  const Router = useRouter();
+  return (
+    <>
+      <h1 className={styles.appName}>
+        <span style={{ color: '#0070f3' }}>Pet</span> Emergency
+      </h1>
+      <h1 className={styles.titleText}>
+        Keep{' '}
+        <span style={{ color: '#0070f3' }}>
+          <strong>calm</strong>
+        </span>
+        . We are here to help.
+      </h1>
+      <div className={styles.container}>
+        <div className={styles.CPRInstructions}>
+          <h2>Choking and CPR</h2>
+          <h3>For</h3>
+          <div
+            className={styles.NearestScrollable + ' ' + styles.buttons}
+            styles={{ overflow: 'hidden' }}
+          >
+            <button className={styles.navigate}>cat</button>
+
+            <button className={styles.navigate}>dog</button>
+          </div>
+          {/* <div className={styles.PictureContainer}>
+            <Image src={instructionImage} width="500px" height="600px" layout="fixed"></Image>
+          </div> */}
+        </div>
+
+        <div className={styles.NearestHospitals}>
+          <h2>
+            Nearest emergency animal hospitals
+            {/* <i class="far fa-compass"></i> */}
+            {/* <i class="fas fa-sync-alt"></i> */}
+            <button
+              className={'fas fa-sync-alt ' + styles.refreshButton}
+              // style={{
+              //   border: 'none',
+              //   background: 'transparent',
+              //   color: '#0070f3',
+              //   marginLeft: '.5em',
+              //   marginBottom: '.5em',
+              // }}
+              onClick={() => {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                  if (position.coords.latitude && position.coords.longitude) {
+                    Router.push({
+                      pathname: `/hospital-near`,
+                      query: {
+                        latitude: `${position.coords.latitude}`,
+                        longitude: `${position.coords.longitude}`,
+                      },
+                    });
+                  }
+                });
+              }}
+            ></button>
+          </h2>
+          <Hospitals hospitalData={hospitalData} />
+        </div>
+      </div>
+    </>
+  );
 }
