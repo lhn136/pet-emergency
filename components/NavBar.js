e4eed4184ac75a135901ce091d36fe7428e0bf0c
Loc Nguyen <lhn136@gmail.com>
2021-08-20 10:34:48 -0700

    [fix] imports

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,33 @@
-import Link from 'next/Link';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from '../styles/Home.module.css';
+
+import logo from '../public/logo.svg';
 
 const Navbar = ({ getLocation }) => {
   return (
     <div className={styles.NavBar}>
-      <Link href="/">
-        <h2 className={styles.NavItem}>
-          <span style={{ color: 'var(--main-blue-color)' }}>Pet</span> Emergency
-        </h2>
+      <Link className={styles.companyLogo} href="/">
+        <a className={styles.NavItem}>
+          <h2>
+            <Image
+              alt="logo"
+              src={logo}
+              width="25px"
+              height="25"
+              layout="fixed"
+              // className={styles.Picture}
+              styles={{ paddingRight: '1em' }}
+            ></Image>
+            <span style={{ color: 'var(--main-blue-color)' }}>Pet</span> Emergency
+          </h2>
+        </a>
       </Link>
       <Link href="/">
-        <h2 className={styles.NavItem}>about us</h2>
+        <a className={styles.NavItem}>
+          <h2>about us</h2>
+        </a>
       </Link>
     </div>
   );
